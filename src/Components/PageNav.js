817a64e897f56ac7438b45c5fcbd63a2218c2801
TechBitsArmy <200305105118@paruluniversity.ac.in>
2024-02-24 21:53:23 +0530

    NavBar Fixed

--- src/Components/PageNav.js
+++ src/Components/PageNav.js
@@ -1,13 +1,20 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
 import Logo from "./Logo";
+import { useState } from "react";
+import { List, X } from "phosphor-react";
 
 function PageNav() {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <nav className={styles.nav}>
       <Logo />
 
-      <ul>
+      <ul className={`${styles.menu} ${isOpen ? styles.open : ""}`}>
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
@@ -26,7 +33,13 @@ function PageNav() {
         <li>
           <NavLink to="/contact-us">Contact</NavLink>
         </li>
+        <button className={`${styles.close}  `} onClick={toggleMenu}>
+          <X />
+        </button>
       </ul>
+      <button className={styles.menuToggle} onClick={toggleMenu}>
+        <List />
+      </button>
     </nav>
   );
 }
