c0ebaf18fcca48bfc32c95fba845c39e9d390ca4
TechBitsArmy <200305105118@paruluniversity.ac.in>
2024-02-24 12:51:22 +0530

    About Page Added

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,11 +1,20 @@
 import React from "react";
 import PageNav from "../Components/PageNav";
 import PageFooter from "../Components/PageFooter";
-
+import HowWeAre from "../Components/HowWeAre";
+import Features from "../Components/OurProducts/Features";
+import Experience from "../Components/Experience";
+import Skills from "../Components/Skills/Skills";
 function AboutUs() {
   return (
     <>
       <PageNav />
+      <div style={{ padding: "0 50px " }}>
+        <Features />
+      </div>
+      <HowWeAre />
+      <Experience />
+      <Skills />
       <PageFooter />
     </>
   );
