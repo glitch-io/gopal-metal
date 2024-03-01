8e009acc3a2290651e0ae1f782875cb922bf6e83
TechBitsArmy <200305105118@paruluniversity.ac.in>
2024-03-01 09:28:04 +0530

    .

--- src/Components/Slider/Carousel.js
+++ src/Components/Slider/Carousel.js
@@ -15,13 +15,12 @@ function UncontrolledExample() {
       {/* Use map function to generate Carousel.Item components */}
       {carouselItems.map((item, index) => (
         <Carousel.Item key={index}>
-          <div className={styles.img}>
-            <img
-              className={`d-block w-100 ${styles.imgHeight}`}
-              src={process.env.PUBLIC_URL + item.imageUrl}
-              alt={item.altText}
-            />
-          </div>
+          <img
+            className={`d-block w-100 ${styles.imgHeight}`}
+            src={process.env.PUBLIC_URL + item.imageUrl}
+            alt={item.altText}
+          />
+
           <div className={styles.heroHeading}>
             <h1>
               Welcome To <span>Nepco Impex LLP</span>
