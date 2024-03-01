58a83abdfe9ab855a98f56685c66131ae8939860
TechBitsArmy <200305105118@paruluniversity.ac.in>
2024-03-01 14:18:52 +0530

    .

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -2,14 +2,17 @@ import React from "react";
 import styles from "./Banner.module.css";
 function Banner({ title }) {
   return (
-    <div
-      className={styles.banner}
-      style={{
-        backgroundImage: `url(${process.env.PUBLIC_URL}/Imgs/banner.jpg)`,
-      }}
-    >
-      <h1>{title}</h1>
-    </div>
+    <>
+      <div style={{ height: "80px", width: "100%", color: "#fff" }}></div>
+      <div
+        className={styles.banner}
+        style={{
+          backgroundImage: `url(${process.env.PUBLIC_URL}/Imgs/banner.jpg)`,
+        }}
+      >
+        <h1>{title}</h1>
+      </div>
+    </>
   );
 }
 
